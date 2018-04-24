fbbc1a8a150dec76322f7f55a03631db4461b6f3
MyPair <vinothcodesnow@gmail.com>
2018-04-25 03:26:43 +0530

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="wrapper">
+    <nav class="navbar navbar-default">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+          <i class="glyphicon glyphicon-time"></i>
+          NAPA Pricing Portal
+        </a>
+        <ul class="nav navbar-nav">
+          <li>
+            <router-link :to="{ name: 'Home' }">Home</router-link>
+          </li>
+          <li>
+            <router-link to="/about">About</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <div class="container">
+      <div class="col-sm-3">
+
+      </div>
+      <div class="col-sm-9">
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
