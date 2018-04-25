4408c25c92baeb213395fa388c588332f33148ae
Vivek Chinathambi <VChinathambi@jdl.com>
2018-04-25 18:49:54 +0530

    modals

---
+++ src/components/About.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="jumbotron">
+    <h1>You are in About section.</h1>
+    <button type="button" class="btn" @click="showModal">
+      Open Modal!
+    </button>
+    <modal v-show="isModalVisible" @close="closeModal">
+        <h3 slot="header">About</h3>
+          <button slot="footer" class="modal-default-button" @click="closeModal" >
+            ok
+          </button>
+    </modal>
+  </div>
+</template>
+
+<script>
+import modal from '@/components/Modal'
+
+export default {
+  name: 'about',
+  components: {
+    modal
+  },
+  data () {
+    return {
+      isModalVisible: false
+    }
+  },
+  methods: {
+    showModal () {
+      this.isModalVisible = true
+    },
+    closeModal () {
+      this.isModalVisible = false
+    }
+  }
+}
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="jumbotron">
+    <h1>You are in Home section.</h1>
+   <button type="button" class="btn" @click="showModal">
+      Open Modal!
+    </button>
+    <modal v-show="isModalVisible" @close="closeModal">
+        <h3 slot="header">Home</h3>
+        <div slot="footer">
+          <button  class="modal-default-button pull-left"  @click="closeModal" >
+            cancel
+          </button>
+          <button  class="modal-default-button" @click="closeModal" >
+            ok
+          </button>
+        </div>
+  </modal>
+  </div>
+</template>
+
+<script>
+import modal from '@/components/Modal'
+
+export default {
+  name: 'home',
+  components: {
+    modal
+  },
+  data () {
+    return {
+      isModalVisible: false
+    }
+  },
+  methods: {
+    showModal () {
+      this.isModalVisible = true
+    },
+    closeModal () {
+      this.isModalVisible = false
+    }
+  }
+}
+</script>
